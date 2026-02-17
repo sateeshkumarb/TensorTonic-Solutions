623a770830eee8caa0a25ddea7548a6ea52cdd7e
Sateesh <sateeshkumarb@yahoo.com>
2026-02-17 16:33:12 +0530

    Update solution for majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -6,7 +6,7 @@ def majority_classifier(y_train, X_test):
     """
     # Write code here
     (vals,counts) = np.unique(y_train,return_counts=True)
-    if sum(counts)==len(counts):
+    if np.all(counts == counts[0]):
         return np.full_like(X_test, y_train[0])
     else:
         idx = counts.argmax()
