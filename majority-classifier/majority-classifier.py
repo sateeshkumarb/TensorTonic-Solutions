8e36a114492fc62850281a67fdb676208533ce9e
Sateesh <sateeshkumarb@yahoo.com>
2026-02-17 16:30:08 +0530

    Add solution for majority-classifier

---
+++ majority-classifier/majority-classifier.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def majority_classifier(y_train, X_test):
+    """
+    Predict the most frequent label in training data for all test samples.
+    """
+    # Write code here
+    (vals,counts) = np.unique(y_train,return_counts=True)
+    if sum(counts)==len(counts):
+        return np.full_like(X_test, y_train[0])
+    else:
+        idx = counts.argmax()
+        return np.full_like(X_test, vals[idx])
