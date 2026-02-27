dc1e8f26473f2edaa4e040a68d1cd0159de8b729
Sateesh <sateeshkumarb@yahoo.com>
2026-02-27 13:45:17 +0530

    Add solution for random-forest-vote

---
+++ random-forest-vote/random-forest-vote.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def random_forest_vote(predictions):
+    """
+    Compute the majority vote from multiple tree predictions.
+    """
+    choices = []
+    preds = zip(*predictions)
+    for p in preds:
+        preds,counts = np.unique(p,return_counts=True)
+        count_matrix = {}
+        for pred,count in zip(preds,counts):
+            count_matrix[int(pred)] = int(count)
+        max_cnt = -1
+        idx = 0
+        for k,v in count_matrix.items():
+            if v > max_cnt:
+                max_cnt = v
+                idx = k
+        choices.append(idx)
+    return choices
+    
+    
