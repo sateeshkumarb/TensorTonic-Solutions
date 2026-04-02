ddc98933e1e2ccaeffd602ea238331ee54ca4f39
Sateesh <sateeshkumarb@yahoo.com>
2026-04-02 14:04:51 +0530

    Add solution for transformers-attention

---
+++ transformer/transformers-attention/transformers-attention.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn.functional as F
+import math
+
+def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:
+    """
+    Compute scaled dot-product attention.
+    """
+    dk = Q.shape[2]
+    qk = torch.matmul(Q, K.transpose(-2, -1))/math.sqrt(dk)
+    vs = F.softmax(qk,dim=-1)
+    val = torch.matmul(vs,V)
+    return val    
